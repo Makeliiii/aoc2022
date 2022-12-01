d4b4f962c200b1fcc2cbca1c4ad0166d9a839492
Mäkeli <joel.m.maenpaa@gmail.com>
2022-12-01 21:48:28 +0200

    first half of first day

---
+++ Day01.hs
@@ -0,0 +1,28 @@
+import Prelude
+import Text.Read (readMaybe)
+
+total :: [Int] -> Int
+total = sum
+
+totalList :: [[Int]] -> [Int]
+totalList = map total
+
+most :: [[Int]] -> Int
+most = maximum . totalList
+
+readMaybeList :: [String] -> [Maybe Int]
+readMaybeList = map readMaybe
+
+splitWhenNothing :: [Maybe Int] -> [[Int]]
+splitWhenNothing xs = pusher xs []
+    where
+        pusher :: [Maybe Int] -> [Int] -> [[Int]]
+        pusher [] _ = []
+        pusher (Nothing:xs) ys = ys : pusher xs []
+        pusher (Just x:xs) ys = pusher xs (x : ys)
+
+main :: IO ()
+main = do
+    calories <- readFile "inputs/Day01.txt" :: IO String
+    let calorieList = splitWhenNothing $ readMaybeList $ lines calories
+    print $ most calorieList
