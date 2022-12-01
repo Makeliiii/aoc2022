556021e93d78ab8be3859eeeda310824dd2b560a
Mäkeli <joel.m.maenpaa@gmail.com>
2022-12-01 23:01:04 +0200

    fancy comments

--- Day01.hs
+++ Day01.hs
@@ -2,15 +2,30 @@ import Prelude
 import Text.Read (readMaybe)
 import Data.List (sort)
 
+--------------------
+--     Part 1     --
+--------------------
 totalList :: [[Int]] -> [Int]
 totalList = map sum
 
 most :: [[Int]] -> Int
 most = maximum . totalList
 
+--------------------
+--     Part 2     --
+--------------------
 readMaybeList :: [String] -> [Maybe Int]
 readMaybeList = map readMaybe
 
+topThreeTotal :: [[Int]] -> Int
+topThreeTotal = sum . take 3 . sortIncreasing
+    where
+        sortIncreasing :: [[Int]] -> [Int]
+        sortIncreasing = reverse . sort . totalList
+
+--------------------
+--     Helper     --
+--------------------
 splitWhenNothing :: [Maybe Int] -> [[Int]]
 splitWhenNothing xs = pusher xs []
     where
@@ -19,12 +34,9 @@ splitWhenNothing xs = pusher xs []
         pusher (Nothing:xs) ys = ys : pusher xs []
         pusher (Just x:xs) ys = pusher xs (x : ys)
 
-topThreeTotal :: [[Int]] -> Int
-topThreeTotal = sum . take 3 . sortIncreasing
-    where
-        sortIncreasing :: [[Int]] -> [Int]
-        sortIncreasing = reverse . sort . totalList
-
+--------------------
+--      Main      --
+--------------------
 main :: IO ()
 main = do
     calories <- readFile "inputs/Day01.txt" :: IO String
