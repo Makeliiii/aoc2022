b58b7598c33a009c61a9006f6c457c7fdf83afa0
Mäkeli <joel.m.maenpaa@gmail.com>
2022-12-01 22:00:21 +0200

    donie

--- Day01.hs
+++ Day01.hs
@@ -1,5 +1,6 @@
 import Prelude
 import Text.Read (readMaybe)
+import Data.List (sort)
 
 total :: [Int] -> Int
 total = sum
@@ -21,8 +22,17 @@ splitWhenNothing xs = pusher xs []
         pusher (Nothing:xs) ys = ys : pusher xs []
         pusher (Just x:xs) ys = pusher xs (x : ys)
 
+topThreeTotal :: [[Int]] -> Int
+topThreeTotal = getTopThree . sortIncreasing
+    where
+        sortIncreasing :: [[Int]] -> [Int]
+        sortIncreasing = reverse . sort . totalList
+        getTopThree :: [Int] -> Int
+        getTopThree (x:y:z:xs) = x + y + z
+
 main :: IO ()
 main = do
     calories <- readFile "inputs/Day01.txt" :: IO String
     let calorieList = splitWhenNothing $ readMaybeList $ lines calories
     print $ most calorieList
+    print $ topThreeTotal calorieList
